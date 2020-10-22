2289f4f854f0f01df81758b5975e4c680013c7ae
Ishan <ishangangji@gmail.com>
2020-10-22 00:48:59 -0400

    Add delete button on edit form

--- app.js
+++ app.js
@@ -59,6 +59,7 @@ app.use(passport.session());
 
 // Creating a instance of a user model
 const userModel = require("./server/models/User");
+const {get} = require("jquery");
 
 // Import user model
 const User = userModel.User;
